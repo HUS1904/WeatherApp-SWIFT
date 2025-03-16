7f21d3b513982836a5ced920b0e7970c44dd7d48
HUS1904 <144718837+HUS1904@users.noreply.github.com>
2025-03-16 21:52:50 +0100

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Hussein Jarrah on 16/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
